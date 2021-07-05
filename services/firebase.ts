e858ff962e0b17a19c137a7083708df752d0c3f2
Supratim <supratim@elearnmarkets.com>
2021-07-05 17:28:16 +0530

    login

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,25 +3,25 @@ import 'firebase/firestore';
 
 // Initialize Firebase
 
-export const FirebaseConfig = {
-    apiKey: 'AIzaSyCV5h4So0i4hU6XDH7FsHgzgTIu9LCi4gI',
-    authDomain: 'marubeni-waste-man.firebaseapp.com',
-    projectId: 'marubeni-waste-man',
-    storageBucket: 'marubeni-waste-man.appspot.com',
-    messagingSenderId: '932265706392',
-    appId: '1:932265706392:web:d09deeb7cab5d414bf21ed',
-    measurementId: 'G-KZ6GK7530F'
-};
-
 // export const FirebaseConfig = {
-//     apiKey: "AIzaSyATJn8HgyMOOvYwtJNuNRFVdD54zFTaBTY",
-//     authDomain: "waste-4e1a2.firebaseapp.com",
-//     projectId: "waste-4e1a2",
-//     storageBucket: "waste-4e1a2.appspot.com",
-//     messagingSenderId: "453937542813",
-//     appId: "1:453937542813:web:6cc84fcd2cc4e712688674",
-//     measurementId: "G-7HK3V8HHPM"
+//     apiKey: 'AIzaSyCV5h4So0i4hU6XDH7FsHgzgTIu9LCi4gI',
+//     authDomain: 'marubeni-waste-man.firebaseapp.com',
+//     projectId: 'marubeni-waste-man',
+//     storageBucket: 'marubeni-waste-man.appspot.com',
+//     messagingSenderId: '932265706392',
+//     appId: '1:932265706392:web:d09deeb7cab5d414bf21ed',
+//     measurementId: 'G-KZ6GK7530F'
 // };
+
+export const FirebaseConfig = {
+    apiKey: "AIzaSyATJn8HgyMOOvYwtJNuNRFVdD54zFTaBTY",
+    authDomain: "waste-4e1a2.firebaseapp.com",
+    projectId: "waste-4e1a2",
+    storageBucket: "waste-4e1a2.appspot.com",
+    messagingSenderId: "453937542813",
+    appId: "1:453937542813:web:6cc84fcd2cc4e712688674",
+    measurementId: "G-7HK3V8HHPM"
+};
 export const Firebase = firebase.initializeApp(FirebaseConfig);
 export const Firestore = firebase.firestore();
 
